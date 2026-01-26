52976cd5c757cdad97a73d576c8d6cf0d25f9304
stasel <2033301+stasel@users.noreply.github.com>
2026-01-26 11:49:49 +0100

    Fixed an issue with test.sh

--- .hyf/test.sh
+++ .hyf/test.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 # Run your test scripts here.
 # Auto grade tool will execute this file within the .hyf working directory.
-echo << EOF > score.json
+# The result should be stored in score.json file with the format shown below.
+cat << EOF > score.json
 {
   "score": 0,
   "pass": true,
